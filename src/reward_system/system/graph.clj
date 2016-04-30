40e3a33e9e0ab116c5b343854718446605a14a95
Henrique Rodrigues <henrique@localhost.localdomain>
2016-04-30 14:27:08 -0300

    Eliminating unnecessary global var and using local atom instead

--- src/reward_system/system/graph.clj
+++ src/reward_system/system/graph.clj
@@ -19,6 +19,7 @@
 	(graph-html [this] "Returns the graph values to be printed in html."))
 
 ; Graph is a map with all nodes in the graph. Each node has a list of nodes linked to itself.
+; Customers is a list of nodes that are already int the graph
 (defrecord Graph [graph-map]
 	GraphOperations
 	(build-graph [this input-file]
@@ -38,27 +39,26 @@
 		@tmp-this))
 
 	(add-customer [this src dst]
-		(def new-graph this)
-		(def new-node (node/build-node src dst))
-		(let [parent-node (.parent new-node)]
-		(if (nil? parent-node)
-			(do
-				(if (cl/not-has? customers (.value new-node))
-						(def customers (cl/add customers (.value new-node))))
-				(def new-graph (->Graph (assoc (.graph-map new-graph) (keyword (str (.value new-node))) new-node))))
-			(do
-				(let [parent ((.graph-map new-graph) (keyword (str parent-node)))]
-					(def new-graph (->Graph
-						(assoc (.graph-map new-graph)
-							(keyword (str parent-node)) (node/add-link parent new-node))))
+		(let [new-graph (atom this) new-node (node/build-node src dst)]
+			(let [parent-node (.parent new-node)]
+			(if (nil? parent-node)
+				(do
 					(if (cl/not-has? customers (.value new-node))
-						(do
-							(def customers (cl/add customers (.value new-node)))
-							(def new-graph (->Graph
-								(assoc (.graph-map new-graph)
-									(keyword (str (.value new-node))) new-node)))))))))
-		(def new-graph (update-parents new-graph new-node))
-		new-graph)
+							(def customers (cl/add customers (.value new-node))))
+					(swap! new-graph #(->Graph (assoc (.graph-map %1) (keyword (str (.value new-node))) new-node))))
+				(do
+					(let [parent ((.graph-map @new-graph) (keyword (str parent-node)))]
+						(swap! new-graph #(->Graph
+							(assoc (.graph-map %1)
+								(keyword (str parent-node)) (node/add-link parent new-node))))
+						(if (cl/not-has? customers (.value new-node))
+							(do
+								(def customers (cl/add customers (.value new-node)))
+								(swap! new-graph #(->Graph
+									(assoc (.graph-map %1)
+										(keyword (str (.value new-node))) new-node)))))))))
+			(swap! new-graph #(update-parents %1 new-node))
+			@new-graph))
 
 	(print! [this]
 		(cl/print! customers)
@@ -67,60 +67,60 @@
 		(println ""))
 
 	(update-parents [this base]
-		(def new-graph this)
-		(if (or
-				(= nil base)
-				(= nil (.parent base))
-				(> (count (.links ((keyword (str (.parent base))) (.graph-map new-graph)))) 1))
-			new-graph
-			(loop [base-value (.value base), parent ((keyword (str (.parent base))), (.graph-map new-graph)) factor 1]
-				(when (not= nil parent)
-				(if (= nil (.parent parent))
-					new-graph
-					(let [next-parent ((keyword (str (.parent parent))) (.graph-map new-graph))]
-						(let [new-parent
-							(node/->Node
-								(.value next-parent)
-								(.parent next-parent)
-								(+ factor (.score next-parent))
-								(.links next-parent))]
-							(def new-graph (->Graph (assoc (.graph-map new-graph) (keyword (str (.value new-parent))) new-parent)))
-							(recur
-								(.value parent)
-								((keyword (str (.parent parent))) (.graph-map new-graph))
-								(* factor 0.5))))))))
-		new-graph)
+		(let [new-graph (atom this)]
+			(if (or
+					(= nil base)
+					(= nil (.parent base))
+					(> (count (.links ((keyword (str (.parent base))) (.graph-map @new-graph)))) 1))
+				@new-graph
+				(loop [base-value (.value base), parent ((keyword (str (.parent base))), (.graph-map @new-graph)) factor 1]
+					(when (not= nil parent)
+					(if (= nil (.parent parent))
+						@new-graph
+						(let [next-parent ((keyword (str (.parent parent))) (.graph-map @new-graph))]
+							(let [new-parent
+								(node/->Node
+									(.value next-parent)
+									(.parent next-parent)
+									(+ factor (.score next-parent))
+									(.links next-parent))]
+								(swap! new-graph #(->Graph (assoc (.graph-map %1) (keyword (str (.value new-parent))) new-parent)))
+								(recur
+									(.value parent)
+									((keyword (str (.parent parent))) (.graph-map @new-graph))
+									(* factor 0.5))))))))
+		@new-graph))
 
 	(print-json [this]
-		(def tmp-list [])
-		(doseq [[value node] (.graph-map this)]
-			(def tmp-list (conj tmp-list node)))
-		(println "{")
-		(def itr-count 1)
-		(let [size (count tmp-list)]
-			(doseq [node (sort-by :score > tmp-list)]
-				(print (str "\t\"" (.value node) "\": " (.score node)))
-				(if (= itr-count size)
-					(println "")
-					(println ","))
-				(def itr-count (inc itr-count))))
-		(println "}"))
+		(let [tmp-list (atom [])]
+			(doseq [[value node] (.graph-map this)]
+				(swap! tmp-list #(conj %1 node)))
+			(println "{")
+			(def itr-count 1)
+			(let [size (count @tmp-list)]
+				(doseq [node (sort-by :score > @tmp-list)]
+					(print (str "\t\"" (.value node) "\": " (.score node)))
+					(if (= itr-count size)
+						(println "")
+						(println ","))
+					(def itr-count (inc itr-count))))
+			(println "}")))
 
 	(rank-html [this]
-		(def tmp-list [])
-		(doseq [[value node] (.graph-map this)]
-			(def tmp-list (conj tmp-list node)))
-		(let [json (atom "")]
-			(swap! json #(str %1 "{</br>" ))
-			(let [itr-count (atom 1) size (count tmp-list)]
-				(doseq [node (sort-by :score > tmp-list)]
-					(swap! json #(str %1 "\"" (.value node) "\": " (.score node)))
-					(if (= @itr-count size)
-						(swap! json #(str %1 "</br>"))
-						(swap! json #(str %1 ",</br>")))
-					(swap! itr-count #(inc %1))))
-			(swap! json #(str %1 "}"))
-			@json))
+		(let [tmp-list (atom [])]
+			(doseq [[value node] (.graph-map this)]
+				(swap! tmp-list #(conj %1 node)))
+			(let [json (atom "")]
+				(swap! json #(str %1 "{</br>" ))
+				(let [itr-count (atom 1) size (count @tmp-list)]
+					(doseq [node (sort-by :score > @tmp-list)]
+						(swap! json #(str %1 "\"" (.value node) "\": " (.score node)))
+						(if (= @itr-count size)
+							(swap! json #(str %1 "</br>"))
+							(swap! json #(str %1 ",</br>")))
+						(swap! itr-count #(inc %1))))
+				(swap! json #(str %1 "}"))
+				@json)))
 
 	(graph-html [this]
 		(let [html (atom "")]
